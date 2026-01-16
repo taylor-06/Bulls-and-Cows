d79eeb3cef8b65c9a1874912a5125342de42205c
Taylor <taylor@Taylors-M4-MacBook-Pro.local>
2026-01-16 15:07:41 +0000

    added comments to the player script

--- Player.cs
+++ Player.cs
@@ -5,10 +5,11 @@ namespace BullsAndCows
         public string userName;
         public int timeElapsed;
 
+        // Constructor for the player object.
         public Player(string userName, int timeElapsed)
         {
-            this.userName = userName;
-            this.timeElapsed = timeElapsed;
+            this.userName = userName; // Gets the username
+            this.timeElapsed = timeElapsed; // Gets the game time elapsed.
         }
     }
 }
